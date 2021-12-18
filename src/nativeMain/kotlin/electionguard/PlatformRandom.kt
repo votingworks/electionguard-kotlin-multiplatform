272a4743088622ac6f3a356dcc976ec7a9e56d81
Dan Wallach <dwallach@rice.edu>
2021-12-17 18:18:06 -0600

    added in /dev/random for native

--- src/nativeMain/kotlin/electionguard/PlatformRandom.kt
+++ src/nativeMain/kotlin/electionguard/PlatformRandom.kt
@@ -1,5 +1,26 @@
 package electionguard
 
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import platform.posix.fclose
+import platform.posix.fopen
+import platform.posix.fread
+
 actual fun randomBytes(length: Int): ByteArray {
-    TODO()
+    val result = ByteArray(length)
+
+    // TODO: figure out how to do this on Windows -- do we need Windows?
+
+    val fp = fopen("/dev/random", "rb")
+        ?: throw UnsupportedOperationException("this platform doesn't seem to have /dev/random")
+
+    val bytesRead = result.usePinned {
+        fread(it.addressOf(0), 1, length.toULong(), fp)
+    }
+
+    if (bytesRead != length.toULong())
+        throw UnsupportedOperationException("we only got $bytesRead of $length bytes")
+
+    fclose(fp)
+    return result
 }
