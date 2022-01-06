985518507dcbd84352d3ac8f72c094a05841801d
Dan Wallach <dwallach@rice.edu>
2022-01-06 15:54:25 -0600

    added base16 support

--- src/jvmMain/kotlin/electionguard/ElGamalBenchmark.kt
+++ src/jvmMain/kotlin/electionguard/ElGamalBenchmark.kt
@@ -13,75 +13,80 @@ import me.tongfei.progressbar.ProgressBarStyle
 
 fun main() {
     val N = 1000
+    println("ElGamal encryption/decryption benchmark, JDK: ${System.getProperty("java.version")}")
 
-    PowRadixOption.values()
-        .forEach { powRadixOption ->
-            println("=======================================================")
-            println("Initializing benchmark for $powRadixOption")
-            val context = runBlocking { productionGroup(powRadixOption) }
+    runBlocking {
+        PowRadixOption.values()
+            .forEach { powRadixOption ->
+                println("=======================================================")
+                println("Initializing benchmark for $powRadixOption")
+                val context = productionGroup(powRadixOption)
 
-            val keypair = elGamalKeyPairFromRandom(context)
-            val nonces = Array(N) { context.randomElementModQ() }
-            val prng = Random(System.nanoTime()) // not secure, but we don't care
-            val messages = Array(N) { prng.nextInt(1000) }
+                val keypair = elGamalKeyPairFromRandom(context)
+                val nonces = Array(N) { context.randomElementModQ() }
+                val prng = Random(System.nanoTime()) // not secure, but we don't care
+                val messages = Array(N) { prng.nextInt(1000) }
 
-            // force the PowRadix tables to be realized before we start the clock
-            messages[0].encrypt(keypair, nonces[0]).decrypt(keypair)
+                // force the PowRadix tables to be realized before we start the clock
+                messages[0].encrypt(keypair, nonces[0]).decrypt(keypair)
 
-            println("Running!")
+                println("Running!")
 
-            var ciphertexts: List<ElGamalCiphertext>
+                var ciphertexts: List<ElGamalCiphertext>
 
-            val encryptionTimeMs = measureTimeMillis {
-                ciphertexts = ProgressBar
-                    .wrap(
-                        (0..N - 1).asIterable().toList(),
-                        ProgressBarBuilder()
-                            .setStyle(ProgressBarStyle.COLORFUL_UNICODE_BLOCK)
-                            .setInitialMax(N.toLong())
-                            .setUpdateIntervalMillis(50)
-                            .setSpeedUnit(ChronoUnit.SECONDS)
-                            .setUnit(" enc", 1L)
-                            .setMaxRenderedLength(100)
-                            .showSpeed()
-                    )
-                    .map { messages[it].encrypt(keypair, nonces[it]) }
+                val encryptionTimeMs = measureTimeMillis {
+                    ciphertexts = ProgressBar
+                        .wrap(
+                            (0..N - 1).asIterable().toList(),
+                            ProgressBarBuilder()
+                                .setStyle(ProgressBarStyle.COLORFUL_UNICODE_BLOCK)
+                                .setInitialMax(N.toLong())
+                                .setUpdateIntervalMillis(50)
+                                .setSpeedUnit(ChronoUnit.SECONDS)
+                                .setUnit(" enc", 1L)
+                                .setMaxRenderedLength(100)
+                                .showSpeed()
+                        )
+                        .map { messages[it].encrypt(keypair, nonces[it]) }
+                }
+                val encryptionTime = encryptionTimeMs / 1000.0
+
+                val ciphertextArray = ciphertexts.toTypedArray()
+
+                var decryptions: List<Int?>
+                val decryptionTimeMs = measureTimeMillis {
+                    decryptions = ProgressBar
+                        .wrap(
+                            (0..N - 1).asIterable().toList(),
+                            ProgressBarBuilder()
+                                .setStyle(ProgressBarStyle.COLORFUL_UNICODE_BLOCK)
+                                .setInitialMax(N.toLong())
+                                .setUpdateIntervalMillis(50)
+                                .setSpeedUnit(ChronoUnit.SECONDS)
+                                .setUnit(" dec", 1L)
+                                .setMaxRenderedLength(100)
+                                .showSpeed()
+                        )
+                        .map { ciphertextArray[it].decrypt(keypair) }
+                }
+                val decryptionTime = decryptionTimeMs / 1000.0
+
+                println(
+                    "ElGamal %.2f encryptions/sec, %.2f decryptions/sec"
+                        .format(N / encryptionTime, N / decryptionTime)
+                )
+
+                if (decryptions.contains(null)) {
+                    println("------- Unexpected decryption failure! -------")
+                    exitProcess(1)
+                }
+
+                val decryptionsNoNull = decryptions.filterNotNull()
+
+                if (decryptionsNoNull != messages.toList()) {
+                    println("------- Unexpected decryption not inverse of encryption! -------")
+                    exitProcess(1)
+                }
             }
-            val encryptionTime = encryptionTimeMs / 1000.0
-
-            val ciphertextArray = ciphertexts.toTypedArray()
-
-            var decryptions: List<Int?>
-            val decryptionTimeMs = measureTimeMillis {
-                decryptions = ProgressBar
-                    .wrap(
-                        (0..N - 1).asIterable().toList(),
-                        ProgressBarBuilder()
-                            .setStyle(ProgressBarStyle.COLORFUL_UNICODE_BLOCK)
-                            .setInitialMax(N.toLong())
-                            .setUpdateIntervalMillis(50)
-                            .setSpeedUnit(ChronoUnit.SECONDS)
-                            .setUnit(" dec", 1L)
-                            .setMaxRenderedLength(100)
-                            .showSpeed()
-                    )
-                    .map { ciphertextArray[it].decrypt(keypair) }
-            }
-            val decryptionTime = decryptionTimeMs / 1000.0
-
-            println("ElGamal %.2f encryptions/sec, %.2f decryptions/sec"
-                .format(N / encryptionTime, N / decryptionTime))
-
-            if (decryptions.contains(null)) {
-                println("------- Unexpected decryption failure! -------")
-                exitProcess(1)
-            }
-
-            val decryptionsNoNull = decryptions.filterNotNull()
-
-            if (decryptionsNoNull != messages.toList()) {
-                println("------- Unexpected decryption not inverse of encryption! -------")
-                exitProcess(1)
-            }
-        }
+    }
 }
