994ed2e71b1694425c3f34edce37dce65a13c7a4
Dan Wallach <dwallach@rice.edu>
2022-04-01 13:51:10 -0500

    basic benchmark

--- src/jvmMain/kotlin/electionguard/core/SimpleBallotBenchmark.kt
+++ src/jvmMain/kotlin/electionguard/core/SimpleBallotBenchmark.kt
@@ -48,7 +48,7 @@ fun main() {
     runBlocking {
         ProductionMode.values()
             .forEach { mode ->
-                PowRadixOption.values().filter { it != PowRadixOption.EXTREME_MEMORY_USE }
+                PowRadixOption.values()
                     .forEach { powRadixOption ->
                         println("=======================================================")
                         println("Initializing benchmark for $powRadixOption, $mode")
