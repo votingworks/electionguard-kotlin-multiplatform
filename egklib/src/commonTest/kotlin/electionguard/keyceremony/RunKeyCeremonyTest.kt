9da1ef51f0dbe00fbb0644da6d629c546b520216
JohnLCaron <jcaron1129@gmail.com>
2023-10-21 16:05:20 -0600

    Minor tweaks of error returns. Add remoteWorkflow test election record.

--- egklib/src/commonTest/kotlin/electionguard/keyceremony/RunKeyCeremonyTest.kt
+++ egklib/src/commonTest/kotlin/electionguard/keyceremony/RunKeyCeremonyTest.kt
@@ -11,12 +11,9 @@ class RunKeyCeremonyTest {
     fun testKeyCeremonyJson() {
         RunTrustedKeyCeremony.main(
             arrayOf(
-                "-in",
-                "src/commonTest/data/startConfigJson",
-                "-trustees",
-                "testOut/keyceremony/testKeyCeremonyJson/private_data/trustees",
-                "-out",
-                "testOut/keyceremony/testKeyCeremonyJson",
+                "-in", "src/commonTest/data/startConfigJson",
+                "-trustees", "testOut/keyceremony/testKeyCeremonyJson/private_data/trustees",
+                "-out", "testOut/keyceremony/testKeyCeremonyJson",
             )
         )
         RunVerifier.runVerifier(productionGroup(), "testOut/keyceremony/testKeyCeremonyJson", 1, true)
@@ -26,12 +23,9 @@ class RunKeyCeremonyTest {
     fun testKeyCeremonyProto() {
         RunTrustedKeyCeremony.main(
             arrayOf(
-                "-in",
-                "src/commonTest/data/startConfigProto",
-                "-trustees",
-                "testOut/keyceremony/testKeyCeremonyProto/private_data/trustees",
-                "-out",
-                "testOut/keyceremony/testKeyCeremonyProto",
+                "-in", "src/commonTest/data/startConfigProto",
+                "-trustees", "testOut/keyceremony/testKeyCeremonyProto/private_data/trustees",
+                "-out", "testOut/keyceremony/testKeyCeremonyProto",
             )
         )
         RunVerifier.runVerifier(productionGroup(), "testOut/keyceremony/testKeyCeremonyProto", 1, true)
