21c354ec5b99062ca3215188099b1d5804d74289
JohnLCaron <jcaron1129@gmail.com>
2023-10-26 12:22:03 -0600

    Switch to using toml for version catalogs.

--- egkliball/build.gradle.kts
+++ egkliball/build.gradle.kts
@@ -1,11 +1,13 @@
 buildscript {
     repositories {
+        gradlePluginPortal()
         mavenCentral()
     }
 }
 
 plugins {
     kotlin("jvm") version "1.9.10"
+    alias(libs.plugins.serialization)
     application
 }
 
@@ -14,17 +16,9 @@ repositories {
 }
 
 dependencies {
-    // implementation(project(":egklib")) // make this depend on egklib being run
-
     implementation(files("../egklib/build/libs/egklib-jvm-2.0.0-SNAPSHOT.jar")) // add the library to the fatJar
-    implementation(libs.kotlin.result)
-    implementation(libs.pbandk)
-    implementation(libs.oshai.logging)
+    implementation(libs.bundles.eglib)
     implementation(libs.logback.classic)
-    implementation(libs.kotlinx.cli)
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.kotlinx.datetime)
-    implementation(libs.kotlinx.coroutines.core)
 }
 
 tasks.register("fatJar", Jar::class.java) {
